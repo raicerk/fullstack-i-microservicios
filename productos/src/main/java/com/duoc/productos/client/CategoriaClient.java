db92bbaa25f0a15f840f55e89024656a0a5fc91c
Juan Mora <raicerk@gmail.com>
2026-06-27 21:13:30 -0400

    feat: se cambia nombre de categoria en feign

--- productos/src/main/java/com/duoc/productos/client/CategoriaClient.java
+++ productos/src/main/java/com/duoc/productos/client/CategoriaClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "categorias-service", url = "${categorias.api.url:http://localhost:8083/api/v1}")
+@FeignClient(name = "categorias", url = "${categorias.api.url:http://localhost:8083/api/v1}")
 public interface CategoriaClient {
 
     @GetMapping("/categorias")
